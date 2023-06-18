1a5190696d2ab44c5b972a97edb9028f1b124e1a
Dilip Kumar Sharma <diliplakshya@gmail.com>
2023-06-18 12:57:12 +0530

    Add multiple build environment

--- app/config/config.py
+++ app/config/config.py
@@ -2,10 +2,10 @@ from pydantic import BaseSettings
 import os
 
 
-base_dir = os.path.abspath(os.path.dirname(__name__))
+base_dir: str = os.path.abspath(os.path.dirname(__name__))
 
 
-class Settings(BaseSettings):
+class DevSettings(BaseSettings):
     SQLALCHEMY_URL: str | None
     JWT_ALGORITHM: str | None
     TOKEN_EXPIRY_TIME: int | None
@@ -24,3 +24,24 @@ class Settings(BaseSettings):
 
     class Config:
         env_file = os.path.join(base_dir, ".env.dev")
+
+
+class AutTestSettings(BaseSettings):
+    SQLALCHEMY_URL: str | None
+    JWT_ALGORITHM: str | None
+    TOKEN_EXPIRY_TIME: int | None
+    SECRET_KEY: str | None
+    PASSWORD_LENGTH: int | None = 10
+    CELERY_BROKER_URL: str
+    CELERY_RESULT_BACKEND: str
+    MAIL_SENDER_NAME: str
+    MAIL_SENDER_EMAIL: str
+    # Configuration file for logging
+    LOG_CONFIG_FILE: str
+    # Directory where logs will be generated.
+    LOGS_DIR: str
+    # Log File name
+    LOG_FILE_NAME: str
+
+    class Config:
+        env_file = os.path.join(base_dir, ".env.aut_test")
--- app/dependencies/config_dependency.py
+++ app/dependencies/config_dependency.py
@@ -1,7 +1,16 @@
+import os
 from functools import lru_cache
-from app.config.config import Settings
+from app.config.config import DevSettings, AutTestSettings
 
+
+environment = os.getenv('BUILD_ENV') or 'development'
+
+
+config_by_name = dict(
+    development = DevSettings(),
+    aut_testing = AutTestSettings()
+)
 
 @lru_cache
 def get_settings():
-    return Settings()
+    return config_by_name[environment]
