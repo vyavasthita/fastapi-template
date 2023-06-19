b317c957bfdd9c3f116131b324798d4f20836349
Dilip Kumar Sharma <diliplakshya@gmail.com>
2023-06-19 13:56:39 +0530

    Add black package

--- backend/app/routers/auth_router.py
+++ backend/app/routers/auth_router.py
@@ -9,7 +9,7 @@ from app.dependencies.config_dependency import get_settings
 auth_router = APIRouter(prefix="/api/auth")
 
 
-@auth_router.post("/token", tags=["auth"], response_model=Token)
+@auth_router.post("/token", tags=["Auth"], response_model=Token)
 def token(user: Annotated[dict, Depends(UserAuthenticator())]) -> Token:
     access_token = AuthService.create_access_token(
         user.get("email"), get_settings().TOKEN_EXPIRY_TIME
